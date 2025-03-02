48b8caf320bd21d9c23bcc7888754ff254b7aedd
Thompson Scrivner <147932620+tscrivner26@users.noreply.github.com>
2025-03-02 16:43:24 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyArIFb0xjWD-k_VHAT2TQo1HMKzT8phra0",
+  authDomain: "classroom-tools-434be.firebaseapp.com",
+  projectId: "classroom-tools-434be",
+  storageBucket: "classroom-tools-434be.firebasestorage.app",
+  messagingSenderId: "765578088838",
+  appId: "1:765578088838:web:fdecf2b43849e9bdff1c07",
+  measurementId: "G-WXKPL1XBZE"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
